fb77556065754320dc5724ff885645545f184f1d
Indunil Peramuna <iperamuna@gmail.com>
2020-01-26 21:04:24 +0530

    :heavy_plus_sign: Dependancy setup

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        \SocialiteProviders\Manager\ServiceProvider::class
+
     ],
 
     /*
@@ -225,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Facebook' => Vinkla\Facebook\Facades\Facebook::class,
     ],
 
 ];
